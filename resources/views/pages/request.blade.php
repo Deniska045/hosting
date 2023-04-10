2f308a702ffc1a858c2eb2cc218c39053c779f63
Deniska045 <denisidv@mail.ru>
2023-04-10 19:30:44 +0300

    delete public v request

--- resources/views/pages/request.blade.php
+++ resources/views/pages/request.blade.php
@@ -5,6 +5,6 @@
 @section('content')
 <div class="request">
     <h2 style="text-align: center">Запрос по VIN</h2>
-    <img src="{{asset('public/img/vin.png')}}" alt="">
+    <img src="{{asset('/img/vin.png')}}" alt="">
 </div>
 @endsection
